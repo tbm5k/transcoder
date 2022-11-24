071d93ce239549ebb0dc6a18a8e41ab36ceaaad1
tbm5k <tedburg5@gmail.com>
2022-11-24 21:40:03 +0300

    🔧 ts initialization

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
+import * as dotenv from "dotenv";
+dotenv.config()
+
 import ffmpeg from "fluent-ffmpeg";
 ffmpeg.setFfprobePath("/ffmpeg-4.4.2/./ffprobe");
 
 const localMovie = "/mnt/c/Users/teddy/Downloads/videoplayback.mp4";
 const hostedMovie = "https://media-fronti.s3.eu-west-2.amazonaws.com/%23MwelekeoniInternet+_+Karibu+to+a+World+of+Possibilities.mp4";
-//download file
-const ev = async () => {
 
-    
-}
-
-ev()
--- src/utils/helpers/uploader.util.ts
+++ src/utils/helpers/uploader.util.ts
@@ -1,30 +1,34 @@
 import aws from 'aws-sdk';
 import fs from 'fs';
 
+type UploadResponse = {
+    data: string | null;
+    error: boolean;
+}
 //set region
 aws.config.update({region: 'eu-west-2'});
 
 const s3 = new aws.S3({
-    accessKeyId: "AKIAXSMZBMHAWL5YCWEB",
-    secretAccessKey: "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",
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
 })
 
 // the filename receives the full path(destination) of the file, this is both the folder name and the file name 
-export const uploadFile = async (imagePath: string, fileName: string, fileType: string): Promise<string> => {
+export const uploadFile = async (imagePath: string, fileName: string, fileType: string): Promise<UploadResponse> => {
     const blob = fs.readFileSync(imagePath)
 
-    const uploadedMedia = await s3.upload({
-        Bucket: "media-in-1080",
-        Key: fileName,
-        Body: blob,
-        ContentType: fileType,
-        ContentDisposition: 'inline'
-    }, err => {
-        if (err) {
-            throw err
-        }
-    }).promise()
-
-    return uploadedMedia.Location
-
+    try{
+        const uploadedMedia = await s3.upload({
+            Bucket: "media-in-1080",
+            Key: fileName,
+            Body: blob,
+            ContentType: fileType,
+            ContentDisposition: 'inline'
+        }).promise()
+        return {error: false, data: uploadedMedia.Location}
+    }catch(error){
+        console.log(error)
+        return {error: true, data: null}
+    }
+    
 }
