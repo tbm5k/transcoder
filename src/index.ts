d87565d7c0c9ec6b3844807e933f94c419960569
tbm5k <tedburg5@gmail.com>
2023-01-02 15:08:48 +0300

    📄 commented out ffmpeg implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import * as dotenv from "dotenv";
-dotenv.config()
+import { consumer } from "./consumer";
+import { rabbitMq } from "./publisher";
+// dotenv.config()
 
-import ffmpeg from "fluent-ffmpeg";
-ffmpeg.setFfprobePath("/ffmpeg-4.4.2/./ffprobe");
+// import ffmpeg from "fluent-ffmpeg";
+// ffmpeg.setFfprobePath("/ffmpeg-4.4.2/./ffprobe");
 
-const localMovie = "/mnt/c/Users/teddy/Downloads/videoplayback.mp4";
-const hostedMovie = "https://media-fronti.s3.eu-west-2.amazonaws.com/%23MwelekeoniInternet+_+Karibu+to+a+World+of+Possibilities.mp4";
+// const localMovie = "/mnt/c/Users/teddy/Downloads/videoplayback.mp4";
+// const hostedMovie = "https://media-fronti.s3.eu-west-2.amazonaws.com/%23MwelekeoniInternet+_+Karibu+to+a+World+of+Possibilities.mp4";
 
+rabbitMq();
+// consumer();
