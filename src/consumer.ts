521b1ef73a77864a41a60bc00713c0a0ba8e0821
tbm5k <tedburg5@gmail.com>
2023-01-18 19:40:59 +0300

    📄 fanout approach to deliver a message to several queues

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,7 +1,8 @@
-import amqp from "amqplib";
+import amqp, { ConsumeMessage } from "amqplib";
 
 (async () => {
     const queueName: string = "jobs";
+    const exchangeName: string = "1080p";
 
     try {
         const amqpServer = "amqp://localhost:5672";
@@ -11,14 +12,28 @@ import amqp from "amqplib";
         we declare the queue because we might start the consumer 
         before the publisher therefore this makes sure that the queue exists
         */
-        await channel.assertQueue(queueName); 
-        await channel.consume(queueName, (message) => {
-            const data = message?.content.toString();
-            console.log("data", data);
+        // await channel.assertQueue(queueName, {durable: true}); 
+        // await channel.prefetch(1); //option allows the worker not be overloaded with messages if it's not completed a work
+        // await channel.consume(queueName, (message) => {
+        //     const data = message?.content.toString();
+        //     console.log("data", data);
 
-            channel.ack(message!);
-            console.log("Acknowledged!");
-        });
+        //     channel.ack(message!);
+        //     console.log("Acknowledged!");
+        // });
+
+        await channel.assertExchange(exchangeName, 'fanout', { durable: false });
+        const assertedQueue = await channel.assertQueue('', { exclusive: true });
+
+        await channel.bindQueue(assertedQueue.queue, exchangeName, '');
+
+        await channel.consume(assertedQueue.queue, (message: ConsumeMessage | null) => {
+            if(message?.content) {
+                console.log(message.content.toString())
+                channel.ack(message);
+            };
+        })
+
     } catch (error) {
         console.log(error);
     }
--- src/publisher.ts
+++ src/publisher.ts
@@ -3,17 +3,23 @@ import amqp from "amqplib";
 (async () => {
 
     const queueName: string = "jobs";
+    const exchaneName: string = "1080p";
     const data: string = process.argv[2];
     try{
         const connection = await amqp.connect("amqp://localhost:5672");
         const channel = await connection.createChannel();
-        await channel.assertQueue(queueName);
-        channel.sendToQueue(queueName, Buffer.from(data));
+        await channel.assertExchange(exchaneName, 'fanout', { durable: false });
 
-        console.log("job added to queue")
-
+        channel.publish(exchaneName, '', Buffer.from(data));
         await channel.close();
         await connection.close();
+        // await channel.assertQueue(queueName, {durable: true});
+        // channel.sendToQueue(queueName, Buffer.from(data), {persistent: true});
+
+        // console.log("job added to queue")
+
+        // await channel.close();
+        // await connection.close();
     }catch(error){
         console.error(error);
     }
